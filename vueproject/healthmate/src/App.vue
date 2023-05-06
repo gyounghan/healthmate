e00c44feb21df76816bfd630f81b5a4402a1dee7
DESKTOP-OVPUAOG\SAMSUNG <hhee0404@github.com>
2023-05-06 18:37:48 +0900

    vue 로그인 페이지 작성

---
+++ vueproject/healthmate/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Find Your Health Mate</h1>
+  <img alt="Vue logo" src="./assets/HealthLogo.png">
+      <div id="login">
+        <input type="text" placeholder="아이디" autofocus>
+      </div>
+      <div>
+        <input type="text" placeholder="비밀번호" autofocus>
+      </div>
+      <div>
+        <button>로그인</button>
+      </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
